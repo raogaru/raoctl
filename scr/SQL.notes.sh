5b4220b54973b4798f6ea25dcce56b0701281021
raogaru <Rao@Raos-Air.home.net>
2015-11-11 13:24:02 -0500

    chg scr/SQL.notes

--- scr/SQL.notes.sh
+++ scr/SQL.notes.sh
@@ -1,8 +1,8 @@
 # ############################################################
-# SQL NOTES FUNCTIONS
+# ${v_class} NOTES FUNCTIONS
 # ############################################################
 # ------------------------------------------------------------
-# SQL NOTES actions
+# ${v_class} NOTES actions
 action_L1="list new show edit delete search collect "
 action_L="$action_L1 $action_L2 $action_L3"
 # ------------------------------------------------------------
@@ -18,20 +18,20 @@ NOTES_DIR=${RC_DIR}/dat
 NOTES_f () {
 x_cmd=$1
 INPUT
-v_file_name=${NOTES_DIR}/SQL.notes.${input1}.txt
+v_file_name=${NOTES_DIR}/${v_class}.notes.${input1}.txt
 [[ $v_check_file != "NO" ]] && [[ ! -f ${v_file_name} ]] && ERROR "File ${v_file_name} not found !"
 ${1} ${v_file_name}
 }
 # ------------------------------------------------------------
 f_notes_list () {
-find ${NOTES_DIR} -type f -name "SQL.notes.*.txt" -print
+find ${NOTES_DIR} -type f -name "${v_class}.notes.*.txt" -print
 }
 # ------------------------------------------------------------
 f_notes_new () {
 INPUT
-v_file_name=${NOTES_DIR}/SQL.notes.${input1}.txt
+v_file_name=${NOTES_DIR}/${v_class}.notes.${input1}.txt
 [[ -f ${v_file_name} ]] && ERROR "File ${v_file_name} already exists !"
-ECHO "SQL_ID:${input1}" > ${v_file_name}
+ECHO "${v_class}.${v_module}: ${input1}" > ${v_file_name}
 }
 # ------------------------------------------------------------
 f_notes_show () {
@@ -46,22 +46,14 @@ f_notes_delete () {
 NOTES_f rm 
 }
 # ------------------------------------------------------------
-f_notes_add () {
-INPUT 3
-v_file_name=${NOTES_DIR}/SQL.notes.${input1}.txt
-[[ ! -f ${v_file_name} ]] && ERROR "File ${v_file_name} not found !"
-vi ${v_file_name}
-ERROR "not coded yet"
-}
-# ------------------------------------------------------------
 f_notes_search () {
 INPUT
-grep -i ${input1} $(find ${NOTES_DIR} -type f -name "SQL.notes.*.txt" -print)
+grep -i ${input1} $(find ${NOTES_DIR} -type f -name "${v_class}.notes.*.txt" -print)
 }
 # ------------------------------------------------------------
 f_notes_collect () {
 INPUT
-v_file_name=${NOTES_DIR}/SQL.notes.${input1}.txt
+v_file_name=${NOTES_DIR}/${v_class}.notes.${input1}.txt
 [[ -f ${v_file_name} ]] && ERROR "File ${v_file_name} already exists !"
 SQLQRY "@sqlnotes ${input1}" >  ${v_file_name}
 }
