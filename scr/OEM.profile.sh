1932e4e336ea306016e6aafdc2ac0527227bb8ae
raogaru <raogaruinc@gmail.com>
2017-03-22 09:23:21 -0400

    add scr/OEM.profile.sh

---
+++ scr/OEM.profile.sh
@@ -0,0 +1,81 @@
+# ############################################################
+# OEM PROFILE FUNCTIONS - Oracle Enterprise Manager Configuration
+# ############################################################
+# ------------------------------------------------------------
+# 		create_tenant
+# 		delete_tenant
+# 		update_tenant_owner
+# 		update_tenant_state
+
+# OEM PROFILE actions
+action_L1="create associate disassociate delete "
+action_L2="modify_desc modify_users include_profiles "
+action_L3="allocate_quota revoke_quota "
+action_L="$action_L1 $action_L2 $action_L3"
+# ------------------------------------------------------------
+# USAGE DATA
+usage_L=" \
+create,profile_name,Create_OEM_User_Profile \
+associate,profile_name:user_names,Associate_OEM_User_Profile_with_comma_seperated_list_of_users \
+disassociate,profile_name:user_names,Dissssociate_OEM_User_Profile_with_comma_seperated_list_of_users \
+delete,profile_name,Delete_OEM_User_Profile \
+modify_desc,profile_name:description,Modify_OEM_User_Profile_Description \
+modify_users,profile_name:user_names,Modify_OEM_User_Profile_List_of_Users \
+include_profiles,profile_name:profile_names,Modify_OEM_User_Profile_to_include_list_of_profiles \
+"
+# ------------------------------------------------------------
+# Global variable overwrites
+EMCLI_HOME=${HOME}/emcli
+
+# ------------------------------------------------------------
+# Module specific environment variables
+
+# ------------------------------------------------------------
+# Module specific common functions
+
+# ------------------------------------------------------------
+f_profile_create () {
+INPUT
+${EMCLI_HOME}/emcli create_user_profile -name="${input1}"
+}
+# ------------------------------------------------------------
+f_profile_associate () {
+INPUT 2
+${EMCLI_HOME}/emcli associate_user_profile -name="${input1}" -users="${input2}"
+}
+# ------------------------------------------------------------
+f_profile_disassociate () {
+INPUT 2
+${EMCLI_HOME}/emcli disassociate_user_profile -name="${input1}" -users="${input2}"
+}
+# ------------------------------------------------------------
+f_profile_delete () {
+INPUT
+${EMCLI_HOME}/emcli delete_user_profile -name="${input1}"
+}
+# ------------------------------------------------------------
+f_profile_modify_desc () {
+INPUT 2
+${EMCLI_HOME}/emcli modify_user_profile -name="${input1} -description=${input2}"
+}
+# ------------------------------------------------------------
+f_profile_modify_users () {
+INPUT 2
+${EMCLI_HOME}/emcli modify_user_profile -name="${input1} -users=${input2}"
+}
+# ------------------------------------------------------------
+f_profile_include_profiles () {
+INPUT 2
+${EMCLI_HOME}/emcli modify_user_profile -name="${input1} -included_profiles=${input2}"
+}
+# ------------------------------------------------------------
+f_user_allocate_quota () {
+INPUT 2
+${EMCLI_HOME}/emcli allocate_quota -assignee_name="${input1}" -assignee_type="profile" -quota="${input2}"
+}
+# ------------------------------------------------------------
+f_user_revoke_quota () {
+INPUT 2
+${EMCLI_HOME}/emcli revoke_quota -assignee_name="${input1}" -assignee_type="profile" -quota="${input2}"
+}
+# ------------------------------------------------------------
