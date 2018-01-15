6961ef8b77ea85d1481b7706b7b834c8a6247ea3
Rao <rao@raogaru.com>
2018-01-15 13:31:32 +0000

    DISK

--- scr/LINUX/DISK/DISK.fs.sh
+++ scr/LINUX/DISK/DISK.fs.sh
@@ -1,8 +1,8 @@
 # ############################################################
-# DISK VG FUNCTIONS - volume group management
+# DISK FS FUNCTIONS - file system management
 # ############################################################
 # ------------------------------------------------------------
-# DISK VG actions
+# DISK FS actions
 action_L1="tab, list create drop enable disable "
 action_L2="xxx yyy zzz "
 action_L3="www "
@@ -10,11 +10,11 @@ action_L="$action_L1 $action_L2 $action_L3"
 # ------------------------------------------------------------
 # USAGE DATA
 usage_L=" \
-list,NONE,List_Volume_Groups \
-create,NONE,Create_Volume_Groups \
-drop,NONE,Drop_Volume_Groups \
-enable,NONE,Enable_Volume_Groups \
-disable,NONE,Disable_Volume_Groups \
+list,NONE,List_File_Systems \
+create,NONE,Create_File_Systems \
+drop,NONE,Drop_File_Systems \
+enable,NONE,Enable_File_Systems \
+disable,NONE,Disable_File_Systems \
 xxx,NONE,test \
 yyy,NONE,test \
 zzz,NONE,test \
@@ -23,8 +23,23 @@ zzz,NONE,test \
 # local variables
 
 # ------------------------------------------------------------
-f_vg_tab () { 
+f_fs_list () { 
 ECHO "not developed yet"
 }
 # ------------------------------------------------------------
+f_fs_create () { 
 ECHO "not developed yet"
+}
+# ------------------------------------------------------------
+f_fs_drop () { 
+ECHO "not developed yet"
+}
+# ------------------------------------------------------------
+f_fs_enable () { 
+ECHO "not developed yet"
+}
+# ------------------------------------------------------------
+f_fs_disable () { 
+ECHO "not developed yet"
+}
+# ------------------------------------------------------------
---
+++ scr/LINUX/DISK/DISK.lv.sh
@@ -0,0 +1,45 @@
+# ############################################################
+# DISK LV FUNCTIONS - logical volume management
+# ############################################################
+# ------------------------------------------------------------
+# DISK LV actions
+action_L1="tab, list create drop enable disable "
+action_L2="xxx yyy zzz "
+action_L3="ppp "
+action_L="$action_L1 $action_L2 $action_L3"
+# ------------------------------------------------------------
+# USAGE DATA
+usage_L=" \
+list,NONE,List_Logical_Volumes \
+create,NONE,Create_Logical_Volumes \
+drop,NONE,Drop_Logical_Volumes \
+enable,NONE,Enable_Logical_Volumes \
+disable,NONE,Disable_Logical_Volumes \
+xxx,NONE,test \
+yyy,NONE,test \
+zzz,NONE,test \
+"
+# ------------------------------------------------------------
+# local variables
+
+# ------------------------------------------------------------
+f_lv_list () { 
+ECHO "not coded yet"
+}
+# ------------------------------------------------------------
+f_lv_create () { 
+ECHO "not coded yet"
+}
+# ------------------------------------------------------------
+f_lv_drop () { 
+ECHO "not coded yet"
+}
+# ------------------------------------------------------------
+f_lv_enable () { 
+ECHO "not coded yet"
+}
+# ------------------------------------------------------------
+f_lv_disable () { 
+ECHO "not coded yet"
+}
+# ------------------------------------------------------------
