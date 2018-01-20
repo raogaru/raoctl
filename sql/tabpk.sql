f02be331f38abec16d7efdb9de6ccf4fab8d1df2
Rao <rao@raogaru.com>
2018-01-20 21:43:36 +0000

    tabpk.sql

--- sql/tabpk.sql
+++ sql/tabpk.sql
@@ -5,4 +5,5 @@ select c.constraint_name , cc.position, cc.column_name
 from dba_constraints c, dba_cons_columns cc
 where c.constraint_name=cc.constraint_name
 and c.table_name=upper('&1')
+and c.constraint_type='P'
 order by cc.position;
