538ee007caba85c64a1c541a319a23b5ee15572c
Rao <rao@raogaru.com>
2018-01-20 22:28:33 +0000

    printcur.sql

--- sql/printcur.sql
+++ sql/printcur.sql
@@ -1,4 +1,4 @@
 set serveroutput on size 10000
-set linesize 200 trims on recsepchar "-"
+set pagesi 0 linesize 200 trims on recsepchar "-"
 define input_query='&1'
 exec print_table('&input_query');
