15cf32fe56cbf2d484d005b37d23181b3c8eba0c
Rao <rao@raogaru.com>
2018-01-20 22:13:45 +0000

    dbinfo.sql

--- sql/dbinfo.sql
+++ sql/dbinfo.sql
@@ -1,2 +1,1 @@
-select name, db_unique_name, database_role, switchover_status, open_mode 
-from v$database;
+select name, db_unique_name, database_role, switchover_status, open_mode from v$database;
