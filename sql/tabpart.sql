3d3abd7ad8384e1867d2253fae89a0ff6b0e7ef0
Rao <rao@raogaru.com>
2018-01-20 21:46:27 +0000

    tabpart.sql

---
+++ sql/tabpart.sql
@@ -0,0 +1,13 @@
+set linesi 200 trimspool on
+col table_owner format a10 
+col table_name format a30 
+col partition_name format a12 
+col tablespace_name format a15 
+col high_value format a50
+
+select table_owner, table_name, partition_position pos, partition_name, subpartition_count sp_cnt, tablespace_name, num_rows 
+from dba_tab_partitions
+--, high_value
+where table_name like upper('&1%')
+order by table_owner, table_name, partition_position
+/
