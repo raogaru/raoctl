030123f01b88bb7e4cf3216bed391e6f46018b64
Rao <rao@raogaru.com>
2018-01-20 21:43:09 +0000

    tabpk.sql

---
+++ sql/tabpk.sql
@@ -0,0 +1,8 @@
+col constraint_name format a30
+col column_name format a30
+col position format 9999
+select c.constraint_name , cc.position, cc.column_name
+from dba_constraints c, dba_cons_columns cc
+where c.constraint_name=cc.constraint_name
+and c.table_name=upper('&1')
+order by cc.position;
