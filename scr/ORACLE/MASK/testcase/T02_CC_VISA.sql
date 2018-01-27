8761d10d227658dc459e8c02ae902cbb22f34397
Rao <rao@raogaru.com>
2018-01-27 23:57:13 +0000

    T02_CC_VISA.sql

--- scr/ORACLE/MASK/testcase/T02_CC_VISA.sql
+++ scr/ORACLE/MASK/testcase/T02_CC_VISA.sql
@@ -5,21 +5,19 @@ drop table T02_CC_VISA ;
 
 create table T02_CC_VISA (
 o char(19),
-ha char(19),
-h4 char(19),
-h6 char(19),
-h8 char(19),
-sa char(19),
-s4 char(19),
-s6 char(19),
-s8 char(19),
-ma char(19),
-m4 char(19),
-m6 char(19),
-m8 char(19)
+hash_all char(19),
+hash_4 char(19),
+hash_6 char(19),
+hash_8 char(19),
+star_all char(19),
+star_4 char(19),
+star_6 char(19),
+star_8 char(19),
+mask_all char(19),
+mask_4 char(19),
+mask_6 char(19),
+mask_8 char(19)
 );
-
-create public synonym T02_CC_VISA for SYS.T02_CC_VISA;
 
 insert /*+ append no logging */ 
 into T02_CC_VISA  (o)
@@ -33,18 +31,18 @@ connect by leveL <= 350;
 commit;
 
 update T02_CC_VISA set
-ha =o,
-h4 =o,
-h6 =o,
-h8 =o,
-sa =o,
-s4 =o,
-s6 =o,
-s8 =o,
-ma =o,
-m4 =o,
-m6 =o,
-m8 =o ;
+hash_all =o,
+hash_4 =o,
+hash_6 =o,
+hash_8 =o,
+star_all =o,
+star_4 =o,
+star_6 =o,
+star_8 =o,
+mask_all =o,
+mask_4 =o,
+mask_6 =o,
+mask_8 =o ;
 commit;
 
 --
