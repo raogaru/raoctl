d4ce3022f3b333daadfb7dd105c2f6d11ec1e9be
Rao <rao@raogaru.com>
2018-01-28 17:46:24 +0000

    DATA.subset.sh

---
+++ scr/ORACLE/DATA/DATA.subset.sh
@@ -0,0 +1,21 @@
+# ############################################################
+# DATA SUBSET FUNCTIONS - to be coded yet
+# ############################################################
+# ------------------------------------------------------------
+# DATA SUBSET actions
+action_L1="list create  "
+action_L2="list_scans "
+action_L3="x "
+action_L="$action_L1 $action_L2 $action_L3"
+# ------------------------------------------------------------
+# USAGE DATA
+usage_L=" \
+list_rs,,List_RuleSets \
+"
+# ------------------------------------------------------------
+# Module specific environment variables
+STREAMS_CONF=${CFG_DIR}/streams.cfg
+STRLOG=${LOG_DIR}/streams_config.log
+STRADM=ADM
+v_debug=0
+# ------------------------------------------------------------
