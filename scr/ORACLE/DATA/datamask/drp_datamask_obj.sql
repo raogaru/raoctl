b18ceae1ce2a1ac3a03259c307bd4907158c991a
Rao <rao@raogaru.com>
2018-01-28 22:26:31 +0000

    datamask/drp_datamask_obj.sql

--- scr/ORACLE/DATA/datamask/drp_datamask_obj.sql
+++ scr/ORACLE/DATA/datamask/drp_datamask_obj.sql
@@ -6,9 +6,9 @@ drop pacakge DATA_MASK_PKG;
 
 drop view DATA_MASK_STATUS ;
 
-drop table DATA_MASK_LIST ;
-
 drop table DATA_MASK_LOG ;
+
+drop table DATA_MASK_LIST ;
 
 drop table DATA_MASK_ALG ;
 
