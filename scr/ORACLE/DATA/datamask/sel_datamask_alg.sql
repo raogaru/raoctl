71ce82820a4f34d08058bb6a49fb5f7ded66f72f
Rao <rao@raogaru.com>
2018-02-03 12:00:28 +0000

    datamask-dir

--- scr/ORACLE/DATA/datamask/sel_datamask_alg.sql
+++
@@ -1,7 +0,0 @@
-set linesi 150 trims on pages 10000 head on echo off feedback on
-col id format 9999999
-col typ format a30
-col alg format a30
-col txt format a100
-select id,typ,alg,txt from data_mask_alg;
-
