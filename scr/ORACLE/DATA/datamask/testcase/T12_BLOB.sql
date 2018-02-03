87502c2a80657d08c32fb10e9044ecc870f27d92
Rao <rao@raogaru.com>
2018-02-03 13:24:10 +0000

    datamask

--- scr/ORACLE/DATA/datamask/testcase/T12_BLOB.sql
+++ scr/ORACLE/DATA/datamask/testcase/T12_BLOB.sql
@@ -12,42 +12,14 @@ variable_size blob
 
 create public synonym T12_BLOB for T12_BLOB;
 
-/*
 insert 
 into T12_BLOB  (o)
+with a as (select 1  from dual connect by level <=1000)
 select 
-dbms_random.string('P',round(dbms_random.value(10,50),0))
-from  dual
-connect by leveL <= 250;
+to_blob(utl_raw.cast_to_raw(dbms_random.string('P',round(dbms_random.value(100,500),0))))
+from a a1, a a2
+where rownum<50;
 commit;
-*/
-
-declare
-   v_clob        clob:=null;
-   v_blob        blob:=null;
-   v_dest_offset integer := 1;
-   v_src_offset  integer := 1;
-   v_warn        integer;
-   v_ctx         integer := dbms_lob.default_lang_ctx;
-begin
-   for idx in 1..5
-   loop
-     v_clob := v_clob || dbms_random.string('p', 1000);
-   end loop;
-   dbms_lob.createtemporary( v_blob, false );
-   dbms_lob.converttoblob(v_blob,
-                          v_clob,
-                          dbms_lob.lobmaxsize,
-                          v_dest_offset,
-                          v_src_offset,
-                          dbms_lob.default_csid,
-                          v_ctx,
-                          v_warn);
-   insert into T12_BLOB (o)
-     select v_blob from dual
-     connect by level <= 250;
-end;
-/
 
 update T12_BLOB set
 empty =o,
