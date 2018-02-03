e523e87b564eb6d8dd9c0ea977f7dcf89cebf97e
Rao <rao@raogaru.com>
2018-02-03 13:23:53 +0000

    DATA.mask.sh

--- scr/ORACLE/DATA/DATA.mask.sh
+++ scr/ORACLE/DATA/DATA.mask.sh
@@ -4,8 +4,8 @@
 # ------------------------------------------------------------
 # DATA MASK actions
 action_L1="cre_obj drp_obj ins_alg show_alg ins_lst show_lst "
-action_L2="val_lst gen_req prep_run run_mask show_log "
-action_L3=" "
+action_L2="val_lst gen_req gen_scr run_mask show_log "
+action_L3="testcase "
 action_L="$action_L1 $action_L2 $action_L3"
 # ------------------------------------------------------------
 # USAGE DATA
@@ -16,90 +16,107 @@ ins_alg,none,Insert_Available_Algorithms \
 show_alg,none,Show_Available_Algorithms \
 ins_lst,none,Load_Data_Masking_Attribute_List \
 show_lst,none,Show_Data_Masking_Attribute_List \
-val_tbl,none,Validate_Masking_List_Table \
+val_lst,none,Validate_Masking_List_Table \
 gen_req,none,Generate_Requirement_Input_File \
-prep_run,none,Prepare_Run_file_from_Requirement_File \
+gen_scr,none,Generate_Masking_SQL_Script_from_Requirement_File \
 run_mask,none,Run_Data_Masking_Routine \
 show_log,none,Show_Execution_Log \
+testcase,none,Execute_a_Test_Case_Masking \
 "
 # ------------------------------------------------------------
 # Global variables
-#v_debug=1
-#rc_SHOW_SQL=YES
-#rc_SHOW_LINE=YES
+v_debug=0
 rc_DATAMASK_SQL_DIR=${v_class_dir}/datamask
+rc_SHOW_DATAMASK_SCR=${rc_SHOW_DATAMASK_SCR:=YES}
 # ------------------------------------------------------------
 # Module specific environment variables
-v_schema=${rc_MASK_SCHEMA:="RAO"}
+rc_DATAMASK_SCHEMA=${rc_DATAMASK_SCHEMA:="RAO"}
 # ------------------------------------------------------------
 DATAMASK_SQLEXEC () {
 SQLNEWF
-SQLLINE "alter session set current_schema=${v_schema};"
+SQLLINE "alter session set current_schema=${rc_DATAMASK_SCHEMA};"
 SQLLINE "@${rc_DATAMASK_SQL_DIR}/${1}"
 SQLEXEC
 }
 # ------------------------------------------------------------
 f_mask_cre_obj () {
-DATAMASK_SQLEXEC cre_datamask_obj.sql
+DATAMASK_SQLEXEC cre_obj.sql
 }
 # ------------------------------------------------------------
 f_mask_drp_obj () {
-DATAMASK_SQLEXEC drp_datamask_obj.sql
+DATAMASK_SQLEXEC drp_obj.sql
 }
 # ------------------------------------------------------------
 f_mask_ins_alg () {
-DATAMASK_SQLEXEC ins_datamask_alg.sql
+DATAMASK_SQLEXEC ins_alg.sql
 }
 # ------------------------------------------------------------
 f_mask_show_alg () {
-DATAMASK_SQLEXEC sel_datamask_alg.sql
+DATAMASK_SQLEXEC sel_alg.sql
 }
 # ------------------------------------------------------------
 f_mask_ins_lst () {
 #INPUT
 #DATAMASK_SQLEXEC ${input1}
-DATAMASK_SQLEXEC ins_datamask_lst.sql
+DATAMASK_SQLEXEC ins_lst.sql
 }
 # ------------------------------------------------------------
 f_mask_show_lst () {
-DATAMASK_SQLEXEC sel_datamask_lst.sql
+DATAMASK_SQLEXEC sel_lst.sql
 }
 # ------------------------------------------------------------
 f_mask_val_lst () {
 SQLNEWF
+SQLLINE "alter session set current_schema=${rc_DATAMASK_SCHEMA};"
 SQLLINE "set serveroutput on size 100000"
-SQLLINE "alter session set current_schema=${v_schema};"
-SQLLINE "exec ${v_schema}.mskpkg.val;"
+SQLLINE "alter session set current_schema=${rc_DATAMASK_SCHEMA};"
+SQLLINE "exec ${rc_DATAMASK_SCHEMA}.mskpkg.val;"
 SQLEXEC
 }
 # ------------------------------------------------------------
 f_mask_gen_req () {
+SQLNEWF
+SQLLINE "alter session set current_schema=${rc_DATAMASK_SCHEMA};"
 ERROR "Not coded yet"
+}
+# ------------------------------------------------------------
+f_mask_gen_scr () {
+INPUT
+v_DATAMASK_REQ=${input1}
+ECHO "REQ ${v_DATAMASK_REQ}.req"
+CHKFILE ${v_DATAMASK_REQ}.req
+
+INCLIB_m
+
+DEBUG "Prepare and Execute DataMask SQL ..."
+SQLNEWF
+SQLLINE "alter session set current_schema=${rc_DATAMASK_SCHEMA};"
+f_Prepare_SQL_from_REQ
+
+ECHO "Masking SQL ${TMPSQL}"
+[[ "${rc_SHOW_DATAMASK_SCR}" = "YES" ]] && cat ${TMPSQL}
+}
+# ------------------------------------------------------------
+f_mask_run_mask () {
+INPUT
+f_mask_gen_scr
+SQLEXEC
 }
 # ------------------------------------------------------------
 f_mask_testcase () {
 INPUT
 v_DATAMASK_REQ=${v_class_dir}/datamask/testcase/${input1}
-[[ ! -f ${v_DATAMASK_REQ}.sql ]] && ERROR "TESTCASE DDL File ${v_DATAMASK_REQ}.sql not found !"
-v_MAIN_DATAMASK_RUN=${TMPDIR}/main_datamask_run.sql
-rm -f ${v_MAIN_DATAMASK_RUN}
-f_Prepare_SQL_from_REQ 
-ECHO "File ${v_MAIN_DATAMASK_RUN}"
+ECHO "Testcase DDL ${v_DATAMASK_REQ}.sql"
+CHKFILE ${v_DATAMASK_REQ}.sql
 
-}
-# ------------------------------------------------------------
-f_mask_prep_run () {
-INPUT
-v_DATAMASK_REQ=${input1}
-v_MAIN_DATAMASK_RUN=${TMPDIR}/main_datamask_run.sql
-INCLIB_m
-rm -f ${v_MAIN_DATAMASK_RUN}
-f_Prepare_SQL_from_REQ 
-ECHO "File ${v_MAIN_DATAMASK_RUN}"
-}
-# ------------------------------------------------------------
-f_mask_run_mask () {
-INPUT
-SQLRUN "${input1}"
+SQLNEWF
+SQLLINE "set feedback off echo off verify off"
+SQLLINE "alter session set current_schema=${rc_DATAMASK_SCHEMA};"
+SQLLINE "@${v_DATAMASK_REQ}.sql"
+#SQLLINE "@${v_class_dir}/datamask/ins_testcase_lst.sql ${rc_DATAMASK_SCHEMA} ${input1}"
+SQLEXEC
+
+input1=${v_DATAMASK_REQ}
+f_mask_run_mask
 }
 # ------------------------------------------------------------
