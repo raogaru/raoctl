cef0264728e909e04ae54b481109577ebef15246
raogaru <raogaruinc@gmail.com>
2021-01-10 19:42:32 -0500

    Create EDB.lib.sh

---
+++ scr/ORACLE/EDB/EDB.lib.sh
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------
+# Global variable overwrites
+rc_EDB_SCHEMA=${rc_EDB_SCHEMA:="RC_EDB"}				# EDB schema name
+rc_EDB_SCHEMA_TABLESPACE=${rc_EDB_SCHEMA_TABLESPACE:="USERS"}		# EDB schema name
+rc_EDB_TABLE_PREFIX=${rc_EDB_TABLE_PREFIX:=EDB}				# EDB table name prefix
+rc_EDB_SCHEMA_SCRIPT_DIR=${rc_EDB_SCHEMA_SCRIPT_DIR:="/tmp"}		# EDB temporary sql scripts dir
+# ------------------------------------------------------------
+# Module specific common functions
+
+# ------------------------------------------------------------
