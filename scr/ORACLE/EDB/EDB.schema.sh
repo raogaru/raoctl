cb202c0aed4665ac1a9524fbf0bbdb0c69003c71
raogaru <raogaruinc@gmail.com>
2021-01-23 07:04:45 -0500

    update EDB.schema.sh

--- scr/ORACLE/EDB/EDB.schema.sh
+++ scr/ORACLE/EDB/EDB.schema.sh
@@ -20,10 +20,11 @@ zz,none,zz_description \
 # ------------------------------------------------------------
 # Module specific environment variables
 rc_EDB_SCHEMA_TABLE_SCRIPT=${rc_EDB_SCHEMA_SCRIPT_DIR}/edb_cretab.sql	# EDB schema create table script
+rc_EDB_DBLINK_SCRIPT=${rc_EDB_SCHEMA_SCRIPT_DIR}/edb_credblink.sql	# EDB schema create dblink script
 rc_EDB_TABLE_COMMON_COLUMN_1="edb_ymd varchar2(10) default to_char(sysdate,'yyyymmdd') not null"
 rc_EDB_TABLE_COMMON_COLUMN_2="edb_name varchar2(10) not null"
-rc_EDB_TABLE_COMMON_COLUMN_3=""
-rc_EDB_TABLE_COMMON_COLUMNS="${rc_EDB_TABLE_COMMON_COLUMN_1},${rc_EDB_TABLE_COMMON_COLUMN_2}"
+rc_EDB_TABLE_COMMON_COLUMN_DEF="${rc_EDB_TABLE_COMMON_COLUMN_1},${rc_EDB_TABLE_COMMON_COLUMN_2}"
+rc_EDB_TABLE_COMMON_COLUMNS="EDB_YMD,EDB_NAME"
 rc_EDB_TABLES_LIST="PROFILES USERS TABLESPACES TABLES INDEXES INIT_PAR"
 # ------------------------------------------------------------
 # Module specific common functions
@@ -35,30 +36,80 @@ TableScript () {
 echo "$*" >> ${rc_EDB_SCHEMA_TABLE_SCRIPT}
 }
 # ------------------------------------------------------------
+f_schema_gen_ddl_dblinks () {
+rm -f ${rc_EDB_DBLINK_SCRIPT}
 
+DEBUG "Generate create DBLINK DDL for EDB schema"
+cat ${v_class_dir}/db.cfg|egrep -v "^#|^$"| while read v_line 
+do
+v_dbname=$(echo ${v_line}|cut -f1 -d":")
+v_hostname=$(echo ${v_line}|cut -f2 -d":")
+v_portnum=$(echo ${v_line}|cut -f3 -d":")
+v_sidname=$(echo ${v_line}|cut -f4 -d":")
+DEBUG "v_dbname=${v_dbname} v_sidname=${v_sidname} v_hostname=${v_hostname} v_portnum=${v_portnum}"
+
+echo "create database link \"${rc_EDB_SCHEMA}\".\"${v_dbname}\" using 'DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=${v_hostname})(PORT=${v_portnum})))(CONNECT_DATA=(SERVICE_NAME=${v_sidname})))' ;" >> ${rc_EDB_DBLINK_SCRIPT}
+
+done
+}
+# ------------------------------------------------------------
+f_schema_gen_ddl_create_table () {
+rm -f ${rc_EDB_SCHEMA_TABLE_SCRIPT}
+TableScript ""
+TableScript "create table ${rc_EDB_SCHEMA}.EDB_YMD (${rc_EDB_TABLE_COMMON_COLUMN_DEF}) tablespace ${rc_EDB_SCHEMA_TABLESPACE};"
+
+DEBUG "Generate create DDL for EDB table creation"
+
+TableScript ""
+TableScript "alter session set current schema=${rc_EDB_SCHEMA};"
+
+cat ${v_class_dir}/schema.cfg|egrep -v "^#|^$"| while read v_line 
+do
+v_tabname=${rc_EDB_TABLE_PREFIX}_$(echo ${v_line}|cut -f1 -d":")
+v_tabsource=$(echo ${v_line}|cut -f2 -d":")
+v_tabpkcols=$(echo ${v_line}|cut -f3 -d":")
+DEBUG "v_tabname=${v_tabname}"
+DEBUG "v_tabsource=${v_tabsource}"
+
+TableScript ""
+TableScript "-- ----------------------------------------------------------------------"
+TableScript "-- ${rc_EDB_TABLE_PREFIX}_${v_tabname}"
+TableScript "-- ----------------------------------------------------------------------"
+TableScript ""
+TableScript "create table ${v_tabname} tablespace ${rc_EDB_SCHEMA_TABLESPACE} as select a.*, b.* from EDB_YMD a, ${v_tabsource} b where rownum<1;"
+
+TableScript ""
+TableScript "create unique index ${v_tabname}_PK on ${v_tabname} (${rc_EDB_TABLE_COMMON_COLUMNS},${v_tabpkcols}) tablespace ${rc_EDB_SCHEMA_TABLESPACE} ;"
+
+TableScript ""
+TableScript "alter table ${v_tabname} add constraint ${rc_EDB_TABLE_PREFIX}_${v_tabname}_PK primary key (${rc_EDB_TABLE_COMMON_COLUMNS},${v_tabpkcols}) ;"
+
+TableScript ""
+TableScript "create or replace force view ${v_tabname}_ROWCNT as "
+TableScript "select ${rc_EDB_TABLE_COMMON_COLUMNS}, count(1) as rowcnt from ${v_tabname} a where edb_ymd >= to_char(trunc(sysdate)-7,'YYYY-MM-DD') group by ${rc_EDB_TABLE_COMMON_COLUMNS} ;"
+
+TableScript ""
+TableScript "create or replace force view ${v_tabname}_DIFF as" 
+TableScript "select a.* from ${v_tabname} a where edb_ymd = (select max(edb_ymd) from EDB_YMD)"
+TableScript "minus"
+TableScript "select b.* from ${v_tabname} b where edb_ymd = (select max(edb_ymd) from EDB_YMD where edb_ymd<(select max(edb_ymd) from EDB_YMD)) ;"
+
+#(select max(edb_ymd) ymd_0 from ${rc_EDB_SCHEMA}.${rc_EDB_TABLE_PREFIX}_${v_tabname})
+#(select max(edb_ymd) ymd_1 from ${rc_EDB_SCHEMA}.${rc_EDB_TABLE_PREFIX}_${v_tabname} where edb_ymd <)
+
+done
+TableScript "-- ----------------------------------------------------------------------"
+}
 # ------------------------------------------------------------
 f_schema_create () {
-rm -f ${rc_EDB_SCHEMA_TABLE_SCRIPT}
 
-TableScript ""
-TableScript "create table ${rc_EDB_SCHEMA}.EDB_TEMP ("
-TableScript "${rc_EDB_TABLE_COMMON_COLUMNS}"
-TableScript ") tablespace ${rc_EDB_SCHEMA_TABLESPACE};"
-
-for v_tabname in ${rc_EDB_TABLES_LIST}
-do
-DEBUG v_tabname=${v_tabname}
-DEBUG v_tabsource=$(cat ${v_class_dir}/schema.cfg | grep "^${v_tabname}:" |cut -f2 -d":")
-TableScript ""
-TableScript "create table ${rc_EDB_SCHEMA}.${rc_EDB_TABLE_PREFIX}_${v_tabname}"
-TableScript "tablespace ${rc_EDB_SCHEMA_TABLESPACE}"
-TableScript "as select a.*, b.*"
-TableScript "from ${rc_EDB_SCHEMA}.EDB_TEMP a, ${v_tabsource} b"
-TableScript "where rownum<1;"
-done
-
+f_schema_gen_ddl_create_table
+cat ${rc_EDB_SCHEMA_TABLE_SCRIPT}
 # execute in database
-SQLRUN "@${rc_EDB_SCHEMA_TABLE_SCRIPT}"
+#SQLRUN "@${rc_EDB_SCHEMA_TABLE_SCRIPT}"
+
+f_schema_gen_ddl_dblinks
+#cat ${rc_EDB_DBLINK_SCRIPT}
 
 }
 # ------------------------------------------------------------
