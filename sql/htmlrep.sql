76e01b46377c9d1bca1f06099dc35336ecdd465f
raogaru <raogaruinc@gmail.com>
2024-06-13 21:27:36 -0400

    Update htmlrep.sql

--- sql/htmlrep.sql
+++ sql/htmlrep.sql
@@ -9,11 +9,11 @@ prompt <title>RAOCTL_HTML_REPORT_TITLE_TAG</title>
 prompt <style type="text/css">
 prompt body {font:bold 10pt Arial; color:Black; background:White;}
 prompt p {font:bold 10pt Arial; color:Red; background:White;}
-prompt h1 {font:bold 20pt Arial; color:Red; background:White; border-bottom:2px solid $cccc99; margin-top:0pt; margin-bottom:0pt; padding:0px 0px 0px 0px;}
-prompt h2 {font:bold 14pt Arial; color:Red; background:White; border-top:2px solid $cccc99; margin-top:0pt; margin-bottom:0pt; padding:0px 0px 0px 0px;}
-prompt h3 {font:bold 10pt Arial; color:Red; background:White; border-top:1px dotted $cccc99; margin-top:0pt; margin-bottom:0pt; padding:0px 0px 0px 0px;}
-prompt h4 {font:8pt Arial; color:Black; background:White; border-bottom:2px solid $cccc99; margin-top:0pt; margin-bottom:0pt; padding:0px 0px 0px 0px;}
-prompt th {font:10pt Arial; color:White; background:White; border-bottom:2px solid $cccc99; margin-top:0pt; margin-bottom:0pt; padding:4px 4px 2px 0px;}
+prompt h1 {font:bold 20pt Arial; color:Red; background:White; border-bottom:2px solid #cccc99; margin-top:0pt; margin-bottom:0pt; padding:0px 0px 0px 0px;}
+prompt h2 {font:bold 14pt Arial; color:Red; background:White; border-top:2px solid #cccc99; margin-top:0pt; margin-bottom:0pt; padding:0px 0px 0px 0px;}
+prompt h3 {font:bold 10pt Arial; color:Red; background:White; border-top:1px dotted #cccc99; margin-top:0pt; margin-bottom:0pt; padding:0px 0px 0px 0px;}
+prompt h4 {font:8pt Arial; color:Black; background:White; border-bottom:2px solid #cccc99; margin-top:0pt; margin-bottom:0pt; padding:0px 0px 0px 0px;}
+prompt th {font:10pt Arial; color:White; background:White; border-bottom:2px solid #cccc99; margin-top:0pt; margin-bottom:0pt; padding:4px 4px 2px 0px;}
 prompt td {font:10pt Arial; color:Black; background:#F0F0F5; vertical-align:top;}
 prompt a {font:bold 10pt Arial; color:Black; background:#F0F0F5; horizontal-align:right; vertical-align:top; margin-top:0pt; margin-bottom:0pt;}
 prompt table.tdiff { border_collapse: collapse; }
